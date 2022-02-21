5717721c575d24c042ea48cd957567f1a91e66f1
Mostafa-Elnabawy <mostafan33997@gmai.com>
2022-02-21 23:20:03 +0200

    modified column types and removed missing values

--- model.py
+++ model.py
@@ -16,4 +16,16 @@ model_data = model_data.withColumn("arr_delay", model_data.arr_delay.cast("integ
 model_data = model_data.withColumn("air_time", model_data.air_time.cast("integer"))
 model_data = model_data.withColumn("month", model_data.month.cast("integer"))
 model_data = model_data.withColumn("plane_year", model_data.plane_year.cast("integer"))
-model_data.show()
+
+# Create the column plane_age
+model_data = model_data.withColumn("plane_age", model_data.year - model_data.plane_year)
+
+# Create is_late
+model_data = model_data.withColumn("is_late", model_data.arr_delay > 0)
+
+# Convert to an integer
+model_data = model_data.withColumn("label", model_data.is_late.cast("integer"))
+
+# Remove missing values
+model_data = model_data.filter("arr_delay is not NULL and dep_delay is not NULL and air_time is not NULL and plane_year is not NULL")
+model_data.show(5)
